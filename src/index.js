4df00733da662fae1f231e68a7853e3ad3d1b7bf
KaiAtDCI <kaiatdci@web.de>
2021-07-08 13:42:44 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Box = (/* ???? */) => {
+const Box = (props) => {
 
-  // ?????
-  
+    if (!props.hide) console.log(props);
+    let bootstrapClass = null;
+    switch (props.type) {
+        case 'blue': bootstrapClass = 'alert alert-primary'; break;
+        case 'red': bootstrapClass = 'alert alert-danger'; break;
+        case 'orange': bootstrapClass = 'alert alert-warning'; break;
+        default:
+            bootstrapClass = 'alert alert-info'
+    }
+
+    return(
+        !props.hide ?
+            <div className={bootstrapClass} role="alert">
+                This is a box - check it out!
+            </div>
+         : null
+    )
 };
 
 // Do not edit below this line
@@ -15,5 +30,4 @@ const jsx = <>
   <Box hide type="red" message="Hidden box" />
 </>;
 const element = document.getElementById("target");
-
 ReactDOM.render(jsx, element);
